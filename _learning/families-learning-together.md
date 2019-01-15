1cd51b20910387f326e0e053d947c4d15db311f1
Wayne Kelly <wayne.kelly@stgeorgeslupset.org.uk>
2019-01-15 14:45:00 +0000

    Update from Forestry.io - Created families-learning-together.md

---
+++ _learning/families-learning-together.md
@@ -0,0 +1,6 @@
+---
+layout: learning
+title: Families Learning Together
+date: 2019-01-15 14:44:56 +0000
+
+---
--- _site/go.sh
+++
@@ -1,2 +0,0 @@
-trap 'kill %1' SIGINT
-bundle exec jekyll serve & sleep 5s; browser-sync start --proxy="127.0.0.1:4000" --files="_site/*.*"
